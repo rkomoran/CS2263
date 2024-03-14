4ba1d74cf280e1ba56a7b95e06f4c7fb7e6d5486
Rinor Komorani <103604250+rkomoran@users.noreply.github.com>
2024-03-14 09:59:47 -0300

    Add files via upload

---
+++ Labs/Lab4CS223Ex1/ex1.c
@@ -0,0 +1,29 @@
+/* ex1.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void dummy_frame()
+{
+    return;
+}
+
+int main(int argc, char * * argv)
+{
+    int i;
+    //   int a[] = {1,2,3,4,5};;
+    int * a;
+    a = malloc(5 * sizeof(int));
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
+    for (i=0; i<5; i++)
+    printf("main: a = %d %p \n", a[i], &a[i]);
+
+    dummy_frame();
+    free(a);
+
+    return EXIT_SUCCESS;
+}
